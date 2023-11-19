ded9d15651d777dea7656d001824fa9f8aab1495
RivaldoPardede <rivaldopardede3@gmail.com>
2023-11-19 22:31:38 +0700

    enable google authentication

---
+++ src/JS/function.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btn-login");
+const iconClose = document.querySelector(".icon-close");
+
+registerLink.addEventListener("click", () => {
+    wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", () => {
+    wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", () => {
+    wrapper.classList.remove("active-popup");
+});
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
