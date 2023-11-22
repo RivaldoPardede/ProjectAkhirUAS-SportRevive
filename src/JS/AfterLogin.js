cf300df34e47ffdbba3715b3f3c27de8610283ca
RivaldoPardede <rivaldopardede3@gmail.com>
2023-11-22 22:14:38 +0700

    javascript FrontEnd

--- src/JS/AfterLogin.js
+++ src/JS/AfterLogin.js
@@ -1,6 +1,28 @@
-const profileContainer = document.querySelector("#profile_settings .container")
-const profile = document.querySelector(".profile")
-const iconClose = document.querySelector(".icon-close")
+const profileContainer = document.querySelector("#profile_settings .container");
+const profile = document.querySelector(".profile");
+const iconClose = document.querySelector(".icon-close");
+const genderOption = document.querySelectorAll(".btn-gender");
+const genderWrapper = document.getElementById("gender")
+const manPic = [
+    "../Assets/Media/Leg/leg.jpg",
+    "../Assets/Media/Abs/abs.jpg",
+    "../Assets/Media/Arm/arm.jpg",
+    "../Assets/Media/Chest/chest.jpg",
+    "../Assets/Media/Shoulder_Back/shoulder.jpg",
+];
+let gender;
+
+for (let i = 0; i < 2; i++) {
+    genderOption[i].addEventListener("click", () => {
+        gender = genderOption[i].value
+        genderWrapper.style.display = "none"
+    })
+}
+
+for (let i = 0; i < 5; i++) {
+    const exercise = document.querySelector(".exercise-" + (i + 1));
+    console.log(exercise.innerText);
+}
 
 profile.addEventListener("click", () => {
     profileContainer.classList.add("active-popup");
@@ -9,7 +31,6 @@ profile.addEventListener("click", () => {
 iconClose.addEventListener("click", () => {
     profileContainer.classList.remove("active-popup");
 });
-
 
 const dayNames = [
     "Sunday",
@@ -38,7 +59,11 @@ const monthNames = [
 let currentDate = new Date();
 function displayDates() {
     document.getElementById("month-year").innerText =
-       dayNames[currentDate.getDay()] + ", " + monthNames[currentDate.getMonth()] + " " + currentDate.getFullYear();
+        dayNames[currentDate.getDay()] +
+        ", " +
+        monthNames[currentDate.getMonth()] +
+        " " +
+        currentDate.getFullYear();
     for (let i = 1; i <= 5; i++) {
         const circle = document.getElementById("circle" + i);
         let date = new Date(currentDate.getTime());
