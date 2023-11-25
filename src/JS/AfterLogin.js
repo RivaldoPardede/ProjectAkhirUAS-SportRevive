5c0d565578e78509e9eb75e27d806b5ffdc2d1f4
RivaldoPardede <rivaldopardede3@gmail.com>
2023-11-25 13:02:58 +0700

    JS on AfterLogin

--- src/JS/AfterLogin.js
+++ src/JS/AfterLogin.js
@@ -2,27 +2,59 @@ const profileContainer = document.querySelector("#profile_settings .container");
 const profile = document.querySelector(".profile");
 const iconClose = document.querySelector(".icon-close");
 const genderOption = document.querySelectorAll(".btn-gender");
-const genderWrapper = document.getElementById("gender")
-const manPic = [
-    "../Assets/Media/Leg/leg.jpg",
-    "../Assets/Media/Abs/abs.jpg",
-    "../Assets/Media/Arm/arm.jpg",
-    "../Assets/Media/Chest/chest.jpg",
-    "../Assets/Media/Shoulder_Back/shoulder.jpg",
+const genderWrapper = document.getElementById("gender");
+const manPict = [
+    "../Assets/Media/Leg/man_leg.jpg",
+    "../Assets/Media/Abs/man_abs.jpg",
+    "../Assets/Media/Arm/man_arm.jpg",
+    "../Assets/Media/Chest/man_chest.jpg",
+    "../Assets/Media/Shoulder_Back/man_shoulder.jpg",
 ];
+const womanPict = [
+    "../Assets/Media/Leg/woman_leg.jpg",
+    "../Assets/Media/Abs/woman_abs.jpg",
+    "../Assets/Media/Arm/woman_arm.jpeg",
+    "../Assets/Media/Chest/woman_chest.jpeg",
+    "../Assets/Media/Shoulder_Back/woman_back.jpg",
+];
+
 let gender;
 
 for (let i = 0; i < 2; i++) {
     genderOption[i].addEventListener("click", () => {
-        gender = genderOption[i].value
-        genderWrapper.style.display = "none"
-    })
+        gender = genderOption[i].value;
+        genderWrapper.style.display = "none";
+        updateBackground();
+        setGenderDropdown();
+    });
 }
 
-for (let i = 0; i < 5; i++) {
-    const exercise = document.querySelector(".exercise-" + (i + 1));
-    console.log(exercise.innerText);
+function updateBackground() {
+    console.log(gender);
+    for (let i = 0; i < 5; i++) {
+        const exercise_box = document.getElementById("box-" + (i + 1));
+        if (gender == "man") {
+            exercise_box.style.backgroundImage = `url(${manPict[i]})`;
+        } else if (gender == "woman") {
+            exercise_box.style.backgroundImage = `url(${womanPict[i]})`;
+        }
+    }
 }
+
+const genderSettings = document.getElementById("gender-option");
+genderSettings.addEventListener("change", () => {
+    gender = genderSettings.value;
+    updateBackground();
+});
+
+function setGenderDropdown() {
+    genderSettings.value = gender;
+}
+
+gender = "Male";
+
+updateBackground();
+setGenderDropdown();
 
 profile.addEventListener("click", () => {
     profileContainer.classList.add("active-popup");
@@ -30,6 +62,11 @@ profile.addEventListener("click", () => {
 
 iconClose.addEventListener("click", () => {
     profileContainer.classList.remove("active-popup");
+});
+
+const settingsIcon = document.getElementsByClassName("nav-link");
+settingsIcon.addEventListener("click", (event) => {
+    event.preventDefault();
 });
 
 const dayNames = [
